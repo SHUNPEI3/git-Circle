626222de769665ac587e891d66f35b09a95fa6c9
EC2 Default User <ec2-user@ip-172-31-33-11.ap-northeast-1.compute.internal>
2023-02-05 05:47:27 +0000

    [Start]EndUserモデル・コントローラー導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+
+  #顧客用サインアップ・ログイン・ログアウト用
+  devise_for :end_users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
   scope module: :public do
     root to: 'homes#top'
+
     get 'about' => 'homes#about'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
