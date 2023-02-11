930252b30d92c6ad5c95f9a691e080f4ebcf48fc
EC2 Default User <ec2-user@ip-172-31-33-11.ap-northeast-1.compute.internal>
2023-02-11 07:43:52 +0000

    [Start]ゲストログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
     sessions: 'public/sessions'
   }
 
+  devise_scope :enduser do
+    post 'guest_sign_in', to: 'public/sessions#guest_sign_in'
+  end
+
   scope module: :public do
     root to: 'homes#top'
     get 'about' => 'homes#about'
