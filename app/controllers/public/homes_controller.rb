607e15e7c86eb5f9762d5aed7f113b873f3f3d5b
EC2 Default User <ec2-user@ip-172-31-33-11.ap-northeast-1.compute.internal>
2023-03-06 04:09:32 +0000

    レイアウト修正(トップページ）

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,8 +1,9 @@
 class Public::HomesController < ApplicationController
   def top
     @end_user = EndUser.new
-    @community_ranks = Community.find(CommunityUser.group(:community_id).order('count(community_id) desc').limit(3).pluck(:community_id))
-    @community_news = Community.all.order(created_at: :desc).limit(3)
+    # @community_ranks = Community.find(CommunityUser.group(:community_id).order('count(community_id) desc').limit(3).pluck(:community_id))
+    @new_communities = Community.all.order(created_at: :desc).limit(5)
+    @categories = Category.all
   end
 
   def about
