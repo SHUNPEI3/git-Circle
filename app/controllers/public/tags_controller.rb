1e64e06e3f2f6d86b3dc145c8cb5c58f5719679f
EC2 Default User <ec2-user@ip-172-31-33-11.ap-northeast-1.compute.internal>
2023-03-10 05:13:04 +0000

    [Fix]コミュニティ公開・非公開設定

--- app/controllers/public/tags_controller.rb
+++ app/controllers/public/tags_controller.rb
@@ -2,7 +2,6 @@ class Public::TagsController < ApplicationController
   def show
     @tag_list = Tag.all.order(id: "DESC").page params[:page]
     @tag = Tag.find(params[:id])
-    @community = @tag.communities.page(params[:page])
-
+    @communities = @tag.communities.page(params[:page])
   end
 end
