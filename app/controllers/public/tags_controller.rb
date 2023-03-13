e25fdf1b69b7be65425112e94d4997a0ce422fb8
EC2 Default User <ec2-user@ip-172-31-33-11.ap-northeast-1.compute.internal>
2023-03-13 13:50:16 +0000

    [Update]ゲストユーザー制限解除

--- app/controllers/public/tags_controller.rb
+++ app/controllers/public/tags_controller.rb
@@ -1,5 +1,6 @@
 class Public::TagsController < ApplicationController
   def show
+    @category_list = Category.all
     @tag_list = Tag.all.order(id: "DESC").page params[:page]
     @tag = Tag.find(params[:id])
     @communities = @tag.communities.page(params[:page])
