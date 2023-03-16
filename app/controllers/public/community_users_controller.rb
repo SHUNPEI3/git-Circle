6bf4ecd6ec9f2d9e8f0bdbbc27a970e1c82f8624
EC2 Default User <ec2-user@ip-172-31-33-11.ap-northeast-1.compute.internal>
2023-03-16 06:04:28 +0000

    [Update]コミュニティ参加・退出の非同期化

--- app/controllers/public/community_users_controller.rb
+++ app/controllers/public/community_users_controller.rb
@@ -1,6 +1,6 @@
 class Public::CommunityUsersController < ApplicationController
   before_action :find_community
-  
+
   def create
     @community_detail = CommunityDetail.find_by(community_id: @community.id)
     @community_user = current_end_user.community_users.new(community_id: @community.id)
@@ -8,16 +8,19 @@ class Public::CommunityUsersController < ApplicationController
     if @community_user.ensure_participation_conditions(@community, @community_detail, current_end_user)
       community_invitation_notification_delete
       @community.community_join_notification(current_end_user)
-      redirect_to community_path(@community), notice: 'コミュニティに参加しました！'
+      flash.now[:notice] = 'コミュニティに参加しました！'
+      render 'community_user'
     else
-      redirect_to request.referer, alert: '参加条件に一致していないため、加入ができません。参加条件をご確認ください。'
+      flash.now[:alert] = '参加条件に一致していないため、加入ができません。参加条件をご確認ください。'
+      render 'community_user'
     end
   end
 
   def destroy
     community_user = CommunityUser.find_by(end_user_id: current_end_user.id, community_id: @community.id)
     community_user.destroy
-    redirect_to request.referer, notice: 'コミュニティから退出しました'
+    flash.now[:notice] = 'コミュニティから退出しました！'
+    render 'community_user'
   end
 
   def evict
