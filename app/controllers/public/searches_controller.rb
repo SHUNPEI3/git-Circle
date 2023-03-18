a42109fe1575397ffc0020273361821c1342d87c
EC2 Default User <ec2-user@ip-172-31-33-11.ap-northeast-1.compute.internal>
2023-03-18 07:20:30 +0000

    [Update]レイアウト修正

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -2,16 +2,15 @@ class Public::SearchesController < ApplicationController
   def search
     @content = params[:content]
     @model = params[:model]
+    @category_list = Category.all
     if @model == "end_user"
       @records = EndUser.search_for(@content)
     elsif @model == "personal_tag"
       @records = PersonalTag.search_for(@content)
     elsif @model == "community"
       @records = Community.search_for(@content)
-      @category_list = Category.all
     elsif @model == "community_tag"
       @records = Tag.search_for(@content)
-      @category_list = Category.all
     end
   end
 end
